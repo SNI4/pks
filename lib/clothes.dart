fe7b8edfcffdd5fd759ccccfdf6bd768f7167dd2
SNI4 <artemhonest@gmail.com>
2024-09-23 17:43:50 +0300

    online shop

---
+++ lib/clothes.dart
@@ -0,0 +1,7 @@
+class Clothing {
+  final String name;
+  final String imageUrl;
+  final double price;
+
+  Clothing({required this.name, required this.imageUrl, required this.price});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,127 +1,150 @@
 import 'package:flutter/material.dart';
+import 'clothes.dart'; // Импортируем второй файл с моделью данных
 
 void main() {
-  runApp(MyApp());
+  runApp(ClothesApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key); 
-
+class ClothesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      title: 'Магазин Одежды',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 230, 240, 250),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black),
+          bodyMedium: TextStyle(color: Colors.black),
+        ),
+      ),
+      home: ClothesList(),
     );
   }
 }
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key); 
-
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  bool rememberMe = false; 
+class ClothesList extends StatelessWidget {
+  final List<Clothing> clothes = [
+   
+        Clothing(
+      name: 'Куртка',
+      imageUrl: '../lib/images/jacket.jpeg',
+      price: 8990, // Цены в рублях
+    ),
+    Clothing(
+      name: 'Джинсы',
+      imageUrl: '../lib/images/jeans.jpeg',
+      price: 13000,
+    ),
+     Clothing(
+      name: 'Футболка',
+      imageUrl: '../lib/images/tshirt.jpeg',
+      price: 1999, 
+    ),
+    Clothing(
+      name: 'Кроссовки',
+      imageUrl: '../lib/images/sneakers.jpeg',
+      price: 70,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Доступная Одежда'),
+      ),
       body: Center(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal: 32.0),
+        child: ListView.builder(
+          itemCount: clothes.length,
+          itemBuilder: (context, index) {
+            return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                side: const BorderSide(color: Colors.black, width: 1),
+              ),
+              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: ListTile(
+                title: Text(clothes[index].name),
+                subtitle:
+                    Text('${clothes[index].price} ₽'), // Формат цен в рублях
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ClothingDetail(clothing: clothes[index]),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ClothingDetail extends StatelessWidget {
+  final Clothing clothing;
+
+  ClothingDetail({required this.clothing});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(clothing.name),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                'Авторизация',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 40.0),
-
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Логин',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                ),
-              ),
-              SizedBox(height: 16.0),
-
-              TextField(
-                obscureText: true, 
-                decoration: InputDecoration(
-                  labelText: 'Пароль',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                ),
-              ),
-              SizedBox(height: 16.0),
-
-              Row(
-                children: [
-                  Checkbox(
-                    value: rememberMe,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        rememberMe = value ?? false;
-                      });
-                    },
-                  ),
-                  Text('Запомнить меня'),
-                ],
-              ),
-              SizedBox(height: 16.0),
-
+              Image.network(clothing.imageUrl),
+              const SizedBox(height: 8),
+              Text(clothing.name,
+                  style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black)),
+              const SizedBox(height: 16),
+              Text('${clothing.price} ₽',
+                  style: const TextStyle(
+                      fontSize: 20, color: Color.fromARGB(255, 13, 14, 13))),
+              const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
-                  print('Войти нажато');
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Купить ${clothing.name}',
+                          style: const TextStyle(color: Colors.black)),
+                      content: const Text('Вы желаете купить эту одежду?',
+                          style: TextStyle(color: Colors.black)),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Да',
+                              style: TextStyle(color: Colors.black)),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Нет',
+                              style: TextStyle(color: Colors.black)),
+                        ),
+                      ],
+                    ),
+                  );
                 },
-                child: Text('Войти'),
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-              ),
-              SizedBox(height: 16.0),
-
-              OutlinedButton(
-                onPressed: () {
-                  print('Регистрация нажата');
-                },
-                child: Text('Регистрация'),
-                style: OutlinedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  side: BorderSide(color: Colors.blue),
-                ),
-              ),
-              SizedBox(height: 16.0),
-
-              TextButton(
-                onPressed: () {
-                  print('Восстановить пароль нажато');
-                },
-                child: Text(
-                  'Восстановить пароль',
-                  style: TextStyle(color: Colors.grey),
-                ),
+                child: const Text('Купить'),
               ),
             ],
           ),
